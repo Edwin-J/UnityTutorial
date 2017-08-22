ff7a6583880b9bb319ec1a5e21db3af2503df363
Edwin-J <gkdhaosl@naver.com>
2017-08-22 12:12:07 +0900

    0822

---
+++ Assets/scene/script/cameraContoller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraContoller : MonoBehaviour {
+
+    public GameObject ball;
+    private Vector3 offset;
+
+	void Start () {
+
+        offset = ( transform.position ) - ( ball.transform.position ); 
+		
+	}
+	
+	void LateUpdate () {
+
+        transform.position = ( ball.transform.position ) + offset;
+
+	}
+}
