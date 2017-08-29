cfd6be079acace261029e14d07b8d5829b665b86
Edwin-J <gkdhaosl@naver.com>
2017-08-29 11:21:09 +0900

    0829 Final

--- Assets/scene/script/playerController.cs
+++ Assets/scene/script/playerController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
     public float speed = 10;
+    private int count;
 
     void Start () {
 
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        setCountText();
+        winText.text = "";
 
     }
 
@@ -22,5 +31,25 @@ public class playerController : MonoBehaviour {
         rb.AddForce(movement * speed);
 
 	}
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("PickUp")) {
+
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+
+        }
+
+    }
+
+    void setCountText() {
+
+        countText.text = "Count : " + count;
+        if (count >= 8)
+            winText.text = "You Win!";
+
+    }
 
 }
